00268441d9210cbb702618e9ece46d90f634b4bc
animeshjain <animesh.jain@housing.com>
2024-03-23 11:41:37 +0530

    Ping Controller

---
+++ src/main/java/com/dishita/kaamchor/KaamchorApplication.java
@@ -0,0 +1,13 @@
+package com.dishita.kaamchor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaamchorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaamchorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dishita/kaamchor/KaamchorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dishita.kaamchor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaamchorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
